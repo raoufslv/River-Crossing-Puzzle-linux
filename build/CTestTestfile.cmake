03d6879ede8ede62afec0a6d02b8fe2682e805be
Raouf <raoufslv09@gmail.com>
2024-01-29 12:01:24 +0100

    adding all the files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rykerdz/Desktop/M2/sim/tp_1
+# Build directory: /home/rykerdz/Desktop/M2/sim/tp_1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
